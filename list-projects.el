2e37f6ac3fd57dae1d91a6f6612d5ed6ae54d33d
Matthew Tromp <matthewktromp@gmail.com>
2025-04-17 16:12:23 -0400

    renaming everything to list-projects

--- list-projects.el
+++ list-projects.el
@@ -9,13 +9,13 @@
 ;; This file is distributed under the terms of the GNU general public
 ;; license v.3
 
-;; Commentary:
+;;; Commentary:
 
-;; List Projects provides an interactive tabulated list of all projects known to
-;; Emacs. It gives you a centralized view of your projects with useful information
-;; and actions.
+;; List Projects provides an interactive tabulated list of all
+;; projects known to Emacs.  It gives you a centralized view of your
+;; projects with useful information and actions.
 ;;
-;; Features:
+;; Features
 ;; - Shows project names, root directories, open buffer counts, and version control status
 ;; - Fast buffer-to-project assignment using an efficient trie-based algorithm
 ;; - Clickable items to:
@@ -33,72 +33,82 @@
 ;; - Regular Emacs commands are executed in the context of the project at point
 ;;
 ;; You can customize the appearance via:
-;; - project-name-column-width
-;; - project-root-column-width
-;; - project-buffers-count-column-width
-;; - project-vc-column-width
+;; - list-projects-project-name-column-width
+;; - list-projects-project-root-column-width
+;; - list-projects-project-buffers-count-column-width
+;; - list-projects-project-vc-column-width
 ;;
 ;; To use a different function for listing projects, set:
-;; - project-listing-function
+;; - list-projects-default-listing-function
 
 ;; TODO
 ;; - Show current compilation status BLOCKED (I don't think that
 ;;    information is even stored?)
 ;; - Fix project menu showing up in buffer list when hitting c-x p c-b
-;;    (list buffers) (might not be possible?)
-;; - Modify project.el to exclude the *Projects* buffer when listing
-;;    buffers in a project
+;;    (list buffers) (might not be possible? requires changes to project.el?)
 
 ;;; Code:
 
 (require 'project)
 
-(defface project-name
+(defface list-projects-project-name
   '((t :inherit link))
   "Face used on project names in the project menu."
   :version "29.0")
 
-(defface project-buffers-count
+(defface list-projects-project-buffers-count
   '((t :inherit link))
   "Face used on the buffer count in the project menu."
   :version "29.0")
 
-(defface project-vc
+(defface list-projects-project-vc
   '((t :inherit link))
-  "Face used on version control system names in the project menu."
+  "Face used on version control system names in the project list."
   :version "29.0")
 
-(defcustom project-name-column-width 30
+(defgroup list-projects nil
+  "List all the projects Emacs knows about."
+  :group 'project)
+
+(defcustom list-projects-project-name-column-width 30
   "Column width for the Project name in the project menu."
   :type 'natnum
+  :group 'project
   :version "29.0")
 
-(defcustom project-root-column-width 40
+(defcustom list-projects-project-root-column-width 40
   "Column width for the Project root in the project menu."
   :type 'natnum
+  :group 'project
   :version "29.0")
 
-(defcustom project-buffers-count-column-width 10
+(defcustom list-projects-project-buffers-count-column-width 10
   "Column width for the Project buffers count in the project menu."
   :type 'natnum
+  :group 'project
   :version "29.0")
 
-(defcustom project-vc-column-width 10
+(defcustom list-projects-project-vc-column-width 10
   "Column width for the Project VC in the project menu."
   :type 'natnum
+  :group 'project
   :version "29.0")
 
-(defcustom project-listing-function #'project-known-projects
-  "Default function to use to get a list of projects."
+(defcustom list-projects-default-listing-function #'list-projects-known-projects
+  "Default function to use to get a list of projects.
+Must return project objects.  You can construct these from project roots
+with `project--find-in-directory."
   :type 'function
+  :group 'project
   :version "29.0")
 
-(defvar-local project-menu-refresh-fun nil
-  "The function to return a list of projects.")
+(defvar-local list-projects-listing-function nil
+  "A function returning the list of projects to be displayed in this buffer.")
 
-(defun project-menu--buffer-predicate (A B)
+(defun list-projects--buffer-predicate (A B)
   "Predicate to sort \"*Projects*\" buffer by number of project buffers.
-This is used for 'tabulated-list-format' in 'project-menu-mode'."
+This is used for `tabulated-list-format' in `list-projects-mode'.
+A and B are the two rows to compare."
   (let ((dA (string-to-number (car (aref (cadr A) 2))))
         (dB (string-to-number (car (aref (cadr B) 2)))))
     (if (= dA dB)
@@ -107,45 +117,58 @@ This is used for 'tabulated-list-format' in 'project-menu-mode'."
           (string< name-A name-B))
       (< dA dB))))
 
-(defun project--ensure-project-menu-mode ()
-  "Signal a user-error if major mode is not `project-menu-mode'."
-  (unless (derived-mode-p 'project-menu-mode)
+(defun list-projects--ensure-project-menu-mode ()
+  "Signal a user-error if major mode is not `list-projects-mode'."
+  (unless (derived-mode-p 'list-projects-mode)
     (user-error "The current buffer is not a Project Menu")))
 
-(defun project-menu--refresh-contents (&optional _arg _noconfirm)
-  (project--ensure-project-menu-mode)
-  (project-menu--refresh))
+(defun list-projects--revert-hook (&optional _arg _noconfirm)
+  "Revert hook for `list-projects-mode'."
+  (list-projects--ensure-project-menu-mode)
+  (list-projects--refresh))
 
-(defun project-menu-jump-to-project (button)
+(defun list-projects-jump-to-project (button)
   "Triggered when clicking/reting on a project name.
-Opens the project's root directory in 'Dired'."
+Opens the project's root directory in `Dired'.
+BUTTON is what the user pressed."
   (let ((proj (button-get button 'project)))
     (pop-to-buffer (dired-noselect (project-root proj)))))
 
-(defun project-menu--list-buffers (button)
-  "Lists buffers for the project on the current row."
-  (interactive nil project-menu-mode)
+(defun list-projects--list-buffers (button)
+  "Lists buffers for the project on the current row.
+BUTTON is what the user pressed."
+  (interactive nil list-projects-mode)
   (let* ((proj (button-get button 'project))
          (project-current-directory-override (project-root proj))
          (default-directory "/")) ;; Set default directory to avoid the Packages buffer showing up in the list of buffers
     (select-window (project-list-buffers))))
 
-(defun project-menu--show-vc-state (button)
+(defun list-projects--show-vc-state (button)
   "Triggered when clicking/reting on the VC system for a project.
+BUTTON is what the user pressed.
+
 Opens the summary view for the corresponding VC system in the
-project's root directory"
-  (interactive nil project-menu-mode)
+project's root directory."
+  (interactive nil list-projects-mode)
   (let ((proj (button-get button 'project))
         (vc (button-get button 'vc)))
     ;; Use magit if magit is installed and this is a git repo
     (if (and (eq vc 'Git)
              (package-installed-p 'magit))
-        (magit-status (project-root proj))
+        (progn (require 'magit)
+               (magit-status (project-root proj)))
       (vc-dir (project-root proj)))))
 
-;; Trie implementation for fast project-buffer lookups
-(defun project-build-trie (projects)
-  "Build a trie from PROJECTS for fast prefix matching using hash tables."
+(defun list-projects--build-trie (projects)
+  "Build a trie from PROJECTS for fast prefix matching.
+
+This is used when figuring out what buffers are in which projects.
+Using a trie works in O((B+P)*L) where B is the number of buffers, P is
+the number of projects and L is the maximum path length of a project's
+root directory.
+
+Using `project-buffers' would take O(B*P), because it iterates over
+`buffer-list'."
   (let ((trie (make-hash-table :test 'equal)))
     (dolist (proj projects)
       (let* ((project-root (expand-file-name (project-root proj)))
@@ -164,9 +187,10 @@ project's root directory"
         (puthash :project proj current-node)))
     trie))
 
-(defun project-for-buffer (buffer trie)
-  "Find project for BUFFER using TRIE-based prefix matching."
-  (let* ((buffer-dir (with-current-buffer buffer 
+(defun list-projects--project-for-buffer (buffer trie)
+  "Find project for BUFFER using trie-based prefix matching.
+TRIE is a tree of projects computed with `list-projects--build-trie'."
+  (let* ((buffer-dir (with-current-buffer buffer
                        (expand-file-name default-directory)))
          ;; Ensure the path ends with a slash
          (normalized-dir (file-name-as-directory buffer-dir))
@@ -183,21 +207,21 @@ project's root directory"
               (setq matched-project project-marker))))))
     matched-project))
 
-(defun group-buffers-by-project (projects)
+(defun list-projects--group-buffers-by-project (projects)
   "Group all buffers by their PROJECTS using the trie-based lookup.
 Returns a hash table mapping projects to lists of buffers."
-  (let ((trie (project-build-trie projects))
+  (let ((trie (list-projects--build-trie projects))
         (project-buffers (make-hash-table :test 'equal)))
     ;; Assign buffers to projects
     (dolist (buffer (buffer-list))
-      (let ((project (project-for-buffer buffer trie)))
+      (let ((project (list-projects--project-for-buffer buffer trie)))
         (when project
-          (puthash project 
+          (puthash project
                    (cons buffer (gethash project project-buffers nil))
                    project-buffers))))
     project-buffers))
 
-(defun project-menu--print-info-simple (proj buffer-map)
+(defun list-projects--print-info-simple (proj buffer-map)
   "Gets info for PROJ using BUFFER-MAP of precomputed buffer assignments."
   (let* ((buffers (seq-filter
                    (lambda (buf)
@@ -213,52 +237,52 @@ Returns a hash table mapping projects to lists of buffers."
             ;; First column: project name
             ;; Links to dired of project root
             (,(project-name proj)
-             face project-name
-             font-lock-face project-name
+             face list-projects-project-name
+             font-lock-face list-projects-project-name
              follow-link t
              project ,proj
-             action project-menu-jump-to-project)
+             action list-projects-jump-to-project)
             ;; Second column: project root
             (,(project-root proj)
              face link
              font-lock-face link
              follow-link t
              project ,proj
-             action project-menu-jump-to-project)
+             action list-projects-jump-to-project)
             ;; Third column: number of open buffers (blank if 0)
             ;; Links to list of open buffers if nonzero
             ,(if (> num-buffers 0) `(,(number-to-string num-buffers)
-                                     face project-buffers-count
-                                     font-lock-face project-buffers-count
+                                     face list-projects-project-buffers-count
+                                     font-lock-face list-projects-project-buffers-count
                                      follow-link t
                                      project ,proj
-                                     action project-menu--list-buffers)
+                                     action list-projects--list-buffers)
                '(""))
             ;; Fourth column: vc
             ,(if vc
                  `(,(symbol-name (cadr vc))
-                   face project-vc
-                   font-lock-face project-vc
+                   face list-projects-project-vc
+                   font-lock-face list-projects-project-vc
                    follow-link t
                    project ,proj
                    vc ,(cadr vc)
-                   action project-menu--show-vc-state)
+                   action list-projects--show-vc-state)
                `("N/A"
-                 face project-vc
-                 font-lock-face project-vc))])))
+                 face list-projects-project-vc
+                 font-lock-face list-projects-project-vc))])))
 
-(defun project-menu--refresh ()
-  "Populates the project menu with all known projects."
-  (let* ((projects (funcall project-menu-refresh-fun))
-         (buffer-map (group-buffers-by-project projects)))
+(defun list-projects--refresh ()
+  "Populate the projects list."
+  (let* ((projects (funcall list-projects-listing-function))
+         (buffer-map (list-projects--group-buffers-by-project projects)))
     (setq tabulated-list-entries
-          (mapcar (lambda (proj) 
-                    (project-menu--print-info-simple proj buffer-map))
+          (mapcar (lambda (proj)
+                    (list-projects--print-info-simple proj buffer-map))
                   projects)))
   (tabulated-list-print t))
 
 
-(defun project-known-projects ()
+(defun list-projects-known-projects ()
   "Return all known projects as project objects."
   (let ((proj-roots (delete-dups (mapcar #'expand-file-name (project-known-project-roots)))))
     ;; Get project object for each of these roots
@@ -267,41 +291,43 @@ Returns a hash table mapping projects to lists of buffers."
                           (if p (list p) nil)))
             proj-roots)))
 
-(define-derived-mode project-menu-mode tabulated-list-mode "Project menu"
+(define-derived-mode list-projects-mode tabulated-list-mode "Project menu"
   :interactive nil
   ;; Set the default directory to whatever is at point
-  (add-hook 'pre-command-hook 'project-menu--set-default-directory nil t)
+  (add-hook 'pre-command-hook 'list-projects--set-default-directory nil t)
   (setq tabulated-list-format
-        `[("Project" ,project-name-column-width t)
-          ("Root" ,project-root-column-width t)
-          ("Buffers" ,project-buffers-count-column-width project-menu--buffer-predicate)
-          ("VC" ,project-vc-column-width t)])
+        `[("Project" ,list-projects-project-name-column-width t)
+          ("Root" ,list-projects-project-root-column-width t)
+          ("Buffers" ,list-projects-project-buffers-count-column-width list-projects--buffer-predicate)
+          ("VC" ,list-projects-project-vc-column-width t)])
   (setq tabulated-list-padding 2)
   (tabulated-list-init-header)
-  (add-hook 'tabulated-list-revert-hook 'project-menu--refresh-contents nil t))
+  (add-hook 'tabulated-list-revert-hook 'list-projects--revert-hook nil t))
 
-(defvar-local project-menu--old-default-directory nil
-  "The original `default-directory' before `project-menu--set-default-directory'.
+(defvar-local list-projects--old-default-directory nil
+  "The original `default-directory' before `list-projects--set-default-directory'.
 
 If this is non-nil, it indicates that
-`project-menu--set-default-directory' has set
+`list-projects--set-default-directory' has set
 `default-directory'; it will set `default-directory' back to this
 if there's no project at point.")
 
-(defun project-menu--set-default-directory ()
-  "Sets the default directory to the root of the project for the row
-currently at point, so that any commands are relative to this project"
+(defun list-projects--set-default-directory ()
+  "Set the default directory to the root of the selected project.
+This is the project corresponding to the row that POINT is currently on.
+This is so that any commands (e.g. `find-file') are relative to this
+project."
   ;; Get the project our point is currently on
   (if-let ((proj (tabulated-list-get-id)))
       ;; Set default-directory to that root
       (progn
-        (unless project-menu--old-default-directory
-          (setq project-menu--old-default-directory default-directory))
+        (unless list-projects--old-default-directory
+          (setq list-projects--old-default-directory default-directory))
         (setq default-directory (file-name-as-directory (project-root proj))))
     ;; If there's no project at point, reset back to the original default-directory
-    (when project-menu--old-default-directory
-      (setq default-directory project-menu--old-default-directory)
-      (setq project-menu--old-default-directory nil))))
+    (when list-projects--old-default-directory
+      (setq default-directory list-projects--old-default-directory)
+      (setq list-projects--old-default-directory nil))))
 
 (defun list-projects ()
   "Create a project menu buffer."
@@ -315,12 +341,11 @@ If PROJECT-LIST-FUN is non-nil, it should be a function that returns
 a list of projects; it means list those projects and no others."
   (let ((buf (get-buffer-create "*Projects*")))
     (with-current-buffer buf
-      (project-menu-mode)
-      (setq project-menu-refresh-fun
+      (list-projects-mode)
+      (setq list-projects-listing-function
             (if project-list-fun
                 project-list-fun
-              #'project-known-projects))
-      (project-menu--refresh-contents))
+              list-projects-default-listing-function)))
     buf))
 
 (provide 'list-projects)
